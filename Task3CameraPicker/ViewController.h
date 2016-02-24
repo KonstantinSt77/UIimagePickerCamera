280da53f11b771acad200ff15432484b805e4ba2
iuser <iuser@macbook-pro-jelvix.local>
2016-02-24 16:07:00 +0200

    Initial Commit

---
+++ Task3CameraPicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Task3CameraPicker
+//
+//  Created by iuser on 24.02.16.
+//  Copyright © 2016 iuser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
